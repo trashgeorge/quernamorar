4cbfa0a713cf322ad14979022322ec8b3db31610
index.george <121786722+trashgeorge@users.noreply.github.com>
2023-10-28 22:23:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function sim() {
+    location.href = "https://www.youtube.com/shorts/zLJsDq8ROGQ";
+}
+
+function desvia(btn) {
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
